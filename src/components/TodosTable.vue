f7ccc28dacc48d3dfd159db921793ec1a9132a44
John Kuefler <john@dev-squared.com>
2020-11-11 17:23:53 -0600

    updates

--- src/components/TodosTable.vue
+++ src/components/TodosTable.vue
@@ -2,12 +2,36 @@
   <div>
     <v-container>
       <h2 class="my-5">Todos</h2>
+
+      <v-row>
+        <v-col col="5">
+          <v-text-field
+            type="date"
+            label="Due Date*"
+            v-model="newTodoForm.dueDate"
+            required/>
+        </v-col>
+        <v-col col="5">
+          <v-text-field
+            label="Description*"
+            v-model="newTodoForm.description"
+            required/>
+        </v-col>
+        <v-col col="2">
+          <v-btn @click="createTodo">Create</v-btn>
+        </v-col>
+      </v-row>
+
       <v-data-table
         :headers="headers"
         :items="todos"
         :items-per-page="5"
         class="elevation-1"
-      ></v-data-table>
+      >
+        <template v-slot:item.actions="{item}">
+          <v-icon small @click="deleteTodo(item)"> mdi-delete </v-icon>
+        </template>
+      </v-data-table>
     </v-container>
   </div>
 </template>
@@ -25,7 +49,13 @@ export default {
       { text: "Due Date", value: "dueDate" },
       { text: "Description", value: "description" },
       { text: "Complete", value: "completed" },
+      { text: "", value: "actions", sortable:false}
     ],
+    newTodoForm: {
+      dueDate: '',
+      description: '',
+      completed: false
+    }
   }),
   methods: {
     async getTodos() {
@@ -34,6 +64,17 @@ export default {
       );
       this.todos = data;
     },
+    async createTodo() {
+      await axios.post('https://jk-express-todo-api.herokuapp.com/api/v1/todos/create', this.newTodoForm);
+      
+      this.newTodoForm.dueDate = '';
+      this.newTodoForm.description = '';
+      await this.getTodos();
+    },
+    async deleteTodo(todo) {
+      await axios.delete('https://jk-express-todo-api.herokuapp.com/api/v1/todos/delete/'+todo._id);
+      await this.getTodos();
+    }
   },
 };
 </script>
